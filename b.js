6ccbc5ed852a2abf95d1b65c237507f2963da47f
gencode5 <115338865+gencode5@users.noreply.github.com>
2022-10-08 13:44:44 +0300

    Update b.js

--- b.js
+++ b.js
@@ -1,4 +1,4 @@
-"hola-440.blogspot.com".includes(location.host) ? null : 
+"hola-440.blogspot.com | https://fileeme.blogspot.com".includes(location.host) ? null : 
     (document.body.innerHTML = `<div style='display: flex; flex-direction: column; gap: 25px; text-align: center; height: 100vh; justify-content: center;'><h1 style='color:red'>عذرا أنت تستخدم نسخة غير مرخصة</h1>
     <p>الرجاء كل الرجاء عدم استخدام النسخ غير المرخصة تقديرا واحتراما لمجهودنا المبذول في تطوير القالب</p>
     <p>ان كنت غير قادر على شراء النسخة المدفوعة تتوفر نسخة مجانية من القالب يمكنك استخدامها مجانا</p>
