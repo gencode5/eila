0e7ffa34439901c3869b90fe9b7c4a1f6d12ad18
gencode5 <115338865+gencode5@users.noreply.github.com>
2022-10-08 13:01:33 +0300

    Update b.js

--- b.js
+++ b.js
@@ -1,1 +1,11 @@
-atob("aHR0cHM6Ly9ob2xhLTQ0MC5ibG9nc3BvdC5jb20v").includes(location.host)? null: ()=>{(document.body.innerHTML = `<h1 style='color:red'>عذرا أنت تستخدم نسخة غير مرخصة</h1><p>الرجاء كل الرجاء عدم استخدام النسخ غير المرخصة تقديرا واحتراما لمجهودنا المبذول في تطوير القالب</p><p>ان كنت غير قادر على شراء النسخة المدفوعة تتوفر نسخة مجانية من القالب يمكنك استخدامها مجانا</p><p>في حال كنت اشتريت القالب وظهرت لك هذه الرسالة الرجاء عدم التعديل على اكواد القالب دون علم ويمكنك التواصل معنا لاي استفسار (للتواصل من خلال المنصة التي قمت بشراء القالب منها)</p>`);document.body.style =" margin: 0 !important; padding: 3% !important; display: flex !important; flex-direction: column !important; gap: 15px !important;text-align:center!important";};vali = true;
+"hola-440.blogspot.com".includes(location.host)
+  ? null
+  : ()=>{
+    (document.body.innerHTML = `<h1 style='color:red'>عذرا أنت تستخدم نسخة غير مرخصة</h1>
+<p>الرجاء كل الرجاء عدم استخدام النسخ غير المرخصة تقديرا واحتراما لمجهودنا المبذول في تطوير القالب</p>
+<p>ان كنت غير قادر على شراء النسخة المدفوعة تتوفر نسخة مجانية من القالب يمكنك استخدامها مجانا</p>
+<p>في حال كنت اشتريت القالب وظهرت لك هذه الرسالة الرجاء عدم التعديل على اكواد القالب دون علم ويمكنك التواصل معنا لاي استفسار (للتواصل من خلال المنصة التي قمت بشراء القالب منها)</p>
+`);
+document.body.style =" margin: 0 !important; padding: 3% !important; display: flex !important; flex-direction: column !important; gap: 15px !important;text-align:center!important";
+  };
+vali = true;
