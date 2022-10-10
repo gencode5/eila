0b984dcf3deefb06715236bf887cff672dc44158
gencode5 <115338865+gencode5@users.noreply.github.com>
2022-10-10 13:58:03 +0300

    Update b.js

--- b.js
+++ b.js
@@ -3,7 +3,7 @@ vali = true;"https://hola-440.blogspot.com | https://fileeme.blogspot.com".inclu
     <p>الرجاء كل الرجاء عدم استخدام النسخ غير المرخصة تقديرا واحتراما لمجهودنا المبذول في تطوير القالب</p>
     <p>ان كنت غير قادر على شراء النسخة المدفوعة تتوفر نسخة مجانية من القالب يمكنك استخدامها مجانا</p>
     <p>في حال كنت اشتريت القالب وظهرت لك هذه الرسالة الرجاء عدم التعديل على اكواد القالب دون علم ويمكنك التواصل معنا لاي استفسار (للتواصل من خلال المنصة التي قمت بشراء القالب منها)</p></div>`);
-let aaa = document.body.innerHTML;
+/*let aaa = document.body.innerHTML;
 document.body.innerHTML = `<a style='display: block !important;
         opacity: 1 !important;
         visibility: visible !important;
@@ -16,6 +16,6 @@ document.body.innerHTML = `<a style='display: block !important;
       alt="Trafica-template"
       title="Trafica Template"
       width="50"
-    /></a>` + aaa;
+    /></a>` + aaa;*/
 
 
