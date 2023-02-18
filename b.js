f90beb8ced56e2b30fc4cfc13eecf16b6a90ee3d
gencode5 <115338865+gencode5@users.noreply.github.com>
2023-02-18 23:21:39 +0200

    Update b.js

--- b.js
+++ b.js
@@ -1,5 +1,8 @@
 "https://qapp-demo.blogspot.com/ | https://hola-440.blogspot.com | https://fileeme.blogspot.com | https://trafica-demo.blogspot.com".includes(location.host) ? null : 
-    (document.body.innerHTML = `<div style='display: flex; flex-direction: column; gap: 25px; text-align: center; height: 100vh; justify-content: center;'><h1 style='color:red'>عذرا أنت تستخدم نسخة غير مرخصة</h1>
+    (document.body.innerHTML = `<div style='display: flex; flex-direction: column; gap: 25px; text-align: center; height: 100vh; justify-content: center;'>
+    <h1 style='color:red'>عذرا أنت تستخدم نسخة غير مرخصة</h1>
     <p>الرجاء كل الرجاء عدم استخدام النسخ غير المرخصة تقديرا واحتراما لمجهودنا المبذول في تطوير القالب</p>
     <p>ان كنت غير قادر على شراء النسخة المدفوعة تتوفر نسخة مجانية من القالب يمكنك استخدامها مجانا</p>
-    <p>في حال كنت اشتريت القالب وظهرت لك هذه الرسالة الرجاء عدم التعديل على اكواد القالب دون علم ويمكنك التواصل معنا لاي استفسار (للتواصل من خلال المنصة التي قمت بشراء القالب منها)</p></div>`);
+    <p>في حال كنت اشتريت القالب وظهرت لك هذه الرسالة الرجاء عدم التعديل على اكواد القالب دون علم ويمكنك التواصل معنا لاي استفسار (للتواصل من خلال المنصة التي قمت بشراء القالب منها)</p>
+    </div>`);
+vali=true;
