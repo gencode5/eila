346d7934283ef622714f9dcae8cc8ba765f711ea
gencode5 <115338865+gencode5@users.noreply.github.com>
2023-02-18 23:06:16 +0200

    Update b.js

--- b.js
+++ b.js
@@ -1,20 +1,5 @@
-vali = true;"https://qapp-demo.blogspot.com/ | https://hola-440.blogspot.com | https://fileeme.blogspot.com | https://trafica-demo.blogspot.com".includes(location.host) ? null : 
+"https://qapp-demo.blogspot.com/ | https://hola-440.blogspot.com | https://fileeme.blogspot.com | https://trafica-demo.blogspot.com".includes(location.host) ? null : 
     (document.body.innerHTML = `<div style='display: flex; flex-direction: column; gap: 25px; text-align: center; height: 100vh; justify-content: center;'><h1 style='color:red'>عذرا أنت تستخدم نسخة غير مرخصة</h1>
     <p>الرجاء كل الرجاء عدم استخدام النسخ غير المرخصة تقديرا واحتراما لمجهودنا المبذول في تطوير القالب</p>
     <p>ان كنت غير قادر على شراء النسخة المدفوعة تتوفر نسخة مجانية من القالب يمكنك استخدامها مجانا</p>
     <p>في حال كنت اشتريت القالب وظهرت لك هذه الرسالة الرجاء عدم التعديل على اكواد القالب دون علم ويمكنك التواصل معنا لاي استفسار (للتواصل من خلال المنصة التي قمت بشراء القالب منها)</p></div>`);
-let newdiv = document.createElement('div')
-newdiv.innerHTML = `<a style='display: block !important;
-opacity: 1 !important;
-visibility: visible !important;
-overflow: visible !important;margin: 0 auto !important; position: relative !important' class='TraficaLink row-x1' href='https://trafica-demo.blogspot.com/' target='_blank'><img style='display: block !important;
-opacity: 1 !important;
-visibility: visible !important;
-overflow: visible !important;
-position: absolute !important; bottom: 14px !important; left: initial; right: inherit !important'
-src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj-47PD765j-uES33YhFvLCyPNycPyO2geK5ENepzYnT3Qk-4J2mm2qpdfUmpCeyJ-Fsy664BvstZQsP-SvRlvK0HSkYkfcE7AQOU3MAgoggDzetHWKFatg5D9OkAGjoaHTtl6tAG-bbTwZfSQsfZqFBkirC0KfcZV1Ob1-QdgxCruRiKr_yDm96FZqiQ/s1600/Asset%201@3x.png"
-alt="Trafica-template"
-title="Trafica Template"
-width="50"
-/></a>`
-document.body.appendChild(newdiv)
