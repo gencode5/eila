ea19bd4c23e347a29fb20089d317969701e3b801
gencode5 <115338865+gencode5@users.noreply.github.com>
2023-02-18 23:03:43 +0200

    Update b.js

--- b.js
+++ b.js
@@ -1,4 +1,4 @@
-vali = true;"https://hola-440.blogspot.com | https://fileeme.blogspot.com | https://trafica-demo.blogspot.com".includes(location.host) ? null : 
+vali = true;"https://qapp-demo.blogspot.com/ | https://hola-440.blogspot.com | https://fileeme.blogspot.com | https://trafica-demo.blogspot.com".includes(location.host) ? null : 
     (document.body.innerHTML = `<div style='display: flex; flex-direction: column; gap: 25px; text-align: center; height: 100vh; justify-content: center;'><h1 style='color:red'>عذرا أنت تستخدم نسخة غير مرخصة</h1>
     <p>الرجاء كل الرجاء عدم استخدام النسخ غير المرخصة تقديرا واحتراما لمجهودنا المبذول في تطوير القالب</p>
     <p>ان كنت غير قادر على شراء النسخة المدفوعة تتوفر نسخة مجانية من القالب يمكنك استخدامها مجانا</p>
