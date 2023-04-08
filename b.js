d53672a71a0050edc0e19530b4c1f6d2cb99535e
gencode5 <115338865+gencode5@users.noreply.github.com>
2023-04-08 06:41:59 +0300

    Update b.js

--- b.js
+++ b.js
@@ -1,4 +1,4 @@
-"https://qapp-demo.blogspot.com | https://www.dzappz.com | https://www.3tours-impact.com | https://www.autoinsuross.com | https://hd2012hd.blogspot.com | https://www.mogrdroid.com | https://hola-440.blogspot.com | https://fileeme.blogspot.com | https://drawdev.blogspot.com | https://trafica-demo.blogspot.com".includes(location.host) ? null : 
+"https://draft.blogger.com/ | https://qapp-demo.blogspot.com | https://www.dzappz.com | https://www.3tours-impact.com | https://www.autoinsuross.com | https://hd2012hd.blogspot.com | https://www.mogrdroid.com | https://hola-440.blogspot.com | https://fileeme.blogspot.com | https://drawdev.blogspot.com | https://trafica-demo.blogspot.com".includes(location.host) ? null : 
     (document.body.innerHTML = `<div style='display: flex; flex-direction: column; gap: 25px; text-align: center; height: 100vh; justify-content: center;'>
     <h1 style='color:red'>عذرا أنت تستخدم نسخة غير مرخصة</h1>
     <p>الرجاء كل الرجاء عدم استخدام النسخ غير المرخصة تقديرا واحتراما لمجهودنا المبذول في تطوير القالب</p>
