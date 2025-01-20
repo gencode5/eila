4c3ae58ccac5947fa3aab34a727cd7e92c58b4fe
gencode5 <115338865+gencode5@users.noreply.github.com>
2025-01-20 15:23:03 +0200

    Update b.js

--- b.js
+++ b.js
@@ -1,3 +1,4 @@
+/*
 "https://draft.blogger.com/ | https://qapp-demo.blogspot.com | https://www.dzappz.com | https://www.3tours-impact.com | https://www.autoinsuross.com | https://hd2012hd.blogspot.com | https://www.mogrdroid.com | https://hola-440.blogspot.com | https://fileeme.blogspot.com | https://drawdev.blogspot.com | https://trafica-demo.blogspot.com | https://www.ios-a.com | https://qapp-dl.blogspot.com".includes(location.host) ? null : 
     (document.body.innerHTML = `<div style='display: flex; flex-direction: column; gap: 25px; text-align: center; height: 100vh; justify-content: center;'>
     <h1 style='color:red'>عذرا أنت تستخدم نسخة غير مرخصة</h1>
@@ -6,3 +7,4 @@
     <p>في حال كنت اشتريت القالب وظهرت لك هذه الرسالة الرجاء عدم التعديل على اكواد القالب دون علم ويمكنك التواصل معنا لاي استفسار (للتواصل من خلال المنصة التي قمت بشراء القالب منها)</p>
     </div>`);
 var vali=true;
+*/
